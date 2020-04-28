fbfb1b8f7257ea3f3d8e9df4e47ecc7a029f0451
Terence <terence@Terences-MacBook-Air.local>
2020-04-27 23:41:52 -0700

    adds mask but need to figure out tensions

--- src/animation/eye-animation.component.tsx
+++ src/animation/eye-animation.component.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { observable, action } from 'mobx';
-import { Stage, Layer, Circle, Line, Text } from 'react-konva';
+import { Stage, Layer, Circle, Line, Text, Group } from 'react-konva';
 import { observer } from 'mobx-react';
 import { KonvaEventObject } from 'konva/types/Node';
 import Konva from 'konva';
+import { Context } from 'konva/types/Context';
 
 const center = {
   x: 70,
@@ -20,94 +21,111 @@ export class EyeAnimation extends React.Component{
   @observable
   private width: number | undefined;
 
+  private stageRef = React.createRef<Stage>();
   private pupilRef = React.createRef<Konva.Circle>();
   private irisRef = React.createRef<Konva.Circle>();
+
+  private stageDimensions = {
+    x: 0,
+    y: 0,
+  };
 
   @action
   public componentDidMount() {
     this.height = this.ref.current?.offsetHeight || 800;
     this.width = this.ref.current?.offsetWidth || 700;
+
+    const relativeMouse = this.stageRef.current?.getStage().getPointerPosition();
+    this.stageDimensions.x = relativeMouse?.x || 0;
+    this.stageDimensions.y = relativeMouse?.y || 0;
   }
 
   private handleIrisMove = (newX: number, newY: number) => {
-    if (!newX || !newY) return;
-    const {x, y} = center;
-
-    const xdiff = newX / x;
-    const ydiff = newY / y;
-
-    const xFlip = xdiff < 1 ? 1 : -1;
-    const yFlip = ydiff >= 1 ? 1 : -1;
-
-    const finalX = x + (xdiff === 0 ? 0 : Math.log(Math.abs(xdiff))) * 10 * xFlip;
-    const finalY = y + (ydiff === 0 ? 0 : Math.log(Math.abs(ydiff))) * 10 * yFlip;
-
-    this.irisRef.current?.to({
-      x: finalX,
-      y: finalY,
-    });
-    console.log(finalX, finalY);
+    this.handleMove(newX, newY, this.irisRef);
   }
 
   private handlePupilMove = (newX: number, newY: number) => {
+    this.handleMove(newX, newY, this.pupilRef);
+  }
+
+  private handleMove = (newX: number, newY: number, ref: React.RefObject<Konva.Circle>) => {
     if (!newX || !newY) return;
     const {x, y} = center;
     
-    const xdiff = newX / x;
-    const ydiff = newY / y;
+    const xdiff = newX - x;
+    const ydiff = newY - y;
 
-    const xFlip = xdiff < 1 ? 1 : -1;
-    const yFlip = ydiff >= 1 ? 1 : -1;
+    const xFlip = xdiff > 0 ? 1 : -1;
+    const yFlip = ydiff > 0 ? 1 : -1;
 
-    const finalX = x + (xdiff === 0 ? 0 : Math.log(Math.abs(xdiff))) * 10 * xFlip;
-    const finalY = y + (ydiff === 0 ? 0 : Math.log(Math.abs(ydiff))) * 10 * yFlip;
+    const xMove = (xdiff === 0 ? 0 : Math.log(Math.abs(xdiff))) / Math.log(10) * 5 * xFlip;
+    const yMove = (ydiff === 0 ? 0 : Math.log(Math.abs(ydiff))) / Math.log(10) * 5 * yFlip;
 
-    this.pupilRef.current?.to({
-      x: finalX,
-      y: finalY,
+    const finalX = Math.min(5, xMove);
+    const finalY = Math.min(5, yMove);
+
+    ref.current?.to({
+      x: x + finalX,
+      y: y + finalY,
     });
   }
 
-  private handleMouseMove = (event: KonvaEventObject<MouseEvent>) => {
-    const { clientX: x, clientY: y } = event.evt;
-    this.handleIrisMove(x, y);
-    this.handlePupilMove(x, y);
+  private handleMouseMove = () => {
+    const relativeMouse = this.stageRef.current?.getStage().getPointerPosition();
+
+    if (!relativeMouse) return;
+
+    this.handleIrisMove(relativeMouse.x, relativeMouse.y);
+    this.handlePupilMove(relativeMouse.x, relativeMouse.y);
+  }
+
+  private clipFn = (ctx: Context) => {
+    ctx.beginPath();
+    ctx.lineTo(10,50);
+    ctx.lineTo(70,75);                           
+    // ctx.bezierCurveTo(70,79);
+    ctx.lineTo(130,50);
+    // ctx.bezierCurveTo(135,50);
+    ctx.lineTo(70,25);
+    // ctx.bezierCurveTo(70,21);
+    ctx.moveTo(10, 50);
+    // ctx.bezierCurveTo(6,50);
+    ctx.closePath();
   }
 
   public render() {
     const {x, y} = center;
     return (
       <div ref={this.ref}>
-        <Stage style={{border: '1px solid black'}} width={150} height={100} onMouseMove={this.handleMouseMove}>
+        <Stage ref={this.stageRef} style={{border: '1px solid black'}} width={150} height={100} onMouseMove={this.handleMouseMove}>
           <Layer>
-            <Text
-              text={`${this.pupilRef.current?.x}. ${this.pupilRef?.current?.y}`}
-              fontSize={16}
-            />
-            <Circle
-              radius={24}
-              x={x}
-              y={y}
-              ref={this.irisRef}
-              stroke="black"
-              fill="brown"
-            />
-            <Circle
-              radius={11}
-              x={x}
-              y={y}
-              ref={this.pupilRef}
-              stroke="black"
-              fill="black"
-            />
-          </Layer>
-          <Layer>
-            <Line
-              points={[10,50, 70,75, 130,50, 70,25]}
-              tension={0.35}
-              closed
-              stroke="black"
-            />
+            <Group
+              clipFunc={this.clipFn}>
+              <Circle
+                radius={24}
+                x={x}
+                y={y}
+                ref={this.irisRef}
+                stroke="black"
+                fill="brown"
+              />
+              <Circle
+                radius={11}
+                x={x}
+                y={y}
+                ref={this.pupilRef}
+                stroke="black"
+                fill="black"
+              />
+            </Group>
+            <Group>
+              <Line
+                points={[10,50, 70,75, 130,50, 70,25]}
+                tension={0.35}
+                closed
+                stroke="black"
+              />              
+            </Group>
           </Layer>
         </Stage>
       </div>

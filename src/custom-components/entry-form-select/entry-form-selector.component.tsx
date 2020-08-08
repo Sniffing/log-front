4897bb9faea6c9f103193bbf087258be07f88d41
Terence <tserence@gmail.com>
2020-08-08 12:16:35 -0700

    area is already trimmed

--- src/custom-components/entry-form-select/entry-form-selector.component.tsx
+++ src/custom-components/entry-form-select/entry-form-selector.component.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 import './entry-form-selector.scss';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
 
+@observer
 export class EntryFormSelector extends React.Component {
+
+  @observable
+  private optionsVisible = false;
+
+  @action
+  private handleMouseEnter = () => {
+    this.optionsVisible = true;
+  }
+
+  private handleMouseLeave = () => {
+    this.optionsVisible = false;
+  }
 
   public render() {
     return (
       <div className="entryFormSelector">
-        <div className="mainSelector"/>
-        <div className="firstChoice"/>
-        <div className="secondChoice"/>
-        <div className="thirdChoice"/>
+        <div className="mainSelector" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}/>
+        {this.optionsVisible && (
+          <>
+            <div className="firstChoice"/>
+            <div className="secondChoice"/>
+            <div className="thirdChoice"/>
+          </>
+        )}
       </div>
     );
   }
